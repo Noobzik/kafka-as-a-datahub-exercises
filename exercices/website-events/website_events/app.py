178bd711e37dd5004144405644e9d80c5d83601b
Jonathan Raffre <jonathan@raff.re>
2021-02-24 22:18:00 +0100

    update code for connection to confluent cloud

--- exercices/website-events/website_events/app.py
+++ exercices/website-events/website_events/app.py
@@ -1,13 +1,24 @@
 import os
+import ssl
 
 import faust
 
-from .models import Visit, Metric
+from .models import Metric, Visit
 from .source import generate_metric, generate_visit
+
+SSL_CONTEXT = ssl.create_default_context(purpose=ssl.Purpose.SERVER_AUTH)
 
 BROKER_HOST = os.environ.get("BROKER_HOST", "localhost")
 
-app = faust.App("website-events", broker=f"kafka://{BROKER_HOST}")
+app = faust.App(
+    "website-events",
+    broker=f"kafka://{BROKER_HOST}",
+    broker_credentials=faust.SASLCredentials(
+        username=os.environ["BROKER_USERNAME"],
+        password=os.environ["BROKER_PASSWORD"],
+        ssl_context=SSL_CONTEXT,
+    ),
+)
 
 visit_topic = app.topic("visits", key_type=str, value_type=Visit)
 metric_topic = app.topic("metrics", key_type=str, value_type=Metric)
